8a5d360d77b83f6f9b68197eda1a4bd6a1b1dfd9
Quentin Victoor <Inox117@users.noreply.github.com>
2020-02-03 23:15:54 +0900

    Create a first version of the Book template. Remove the Queryable trait because Field names have no effects when Queryable is derived and it was swapping some columns in the output.

--- src/books/mod.rs
+++ src/books/mod.rs
@@ -1,8 +1,11 @@
 pub mod handlers;
 pub mod repository;
 use super::schema::books;
+use diesel::deserialize::Queryable;
 
-#[derive(Queryable, AsChangeset, Identifiable, Debug, PartialEq, Serialize, Deserialize)]
+type DB = diesel::pg::Pg;
+
+#[derive(AsChangeset, Identifiable, Debug, PartialEq, Serialize, Deserialize)]
 #[table_name = "books"]
 pub struct Book {
     id: i32,
@@ -16,3 +19,23 @@ pub struct Book {
     publisher: String,
     number_of_page: i32,
 }
+
+impl Queryable<books::SqlType, DB> for Book {
+    type Row = (i32, String, String, String, String, String, String, String, String, i32);
+
+    fn build(row: Self::Row) -> Self {
+        Book {
+            id: row.0,
+            isbn10: row.1,
+            isbn13: row.2,
+            title: row.3,
+            author: row.4,
+            illustrator: row.5,
+            summary: row.6,
+            publisher: row.7,
+            publication_date: row.8,
+            number_of_page: row.9
+        }
+    }
+}
+
